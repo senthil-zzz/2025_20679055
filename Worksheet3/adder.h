6b2f450102dd2a8e8e445600075d57b63cb8362f
Senthil <senthilvelanm2006@gmail.com>
2026-02-10 16:52:09 +0000

    Updated header for DLL support and built dynamic lib

--- Worksheet3/adder.h
+++ Worksheet3/adder.h
@@ -1,4 +1,25 @@
 #ifndef MATHSLIB_ADDER_H
 #define MATHSLIB_ADDER_H
-int add(int a, int b);
+
+// check OS for Windows
+#if defined(WIN32)|defined(_WIN32)
+#ifdef maths_STATIC
+
+#define MATHSLIB_API
+#else
+#ifdef maths_EXPORTS
+  // if BUILDING the DLL, then EXPORT
+#define MATHSLIB_API __declspec(dllexport)
+#else
+  // If USING the DLL, IMPORT
+#define MATHSLIB_API __declspec(dllimport)
+#endif
+#endif
+#else
+
+#define MATHSLIB_API
+#endif
+
+MATHSLIB_API int add(int a, int b);
+
 #endif
---
+++ Worksheet3/adder_static/include/adder.h
@@ -0,0 +1,4 @@
+#ifndef MATHSLIB_ADDER_H
+#define MATHSLIB_ADDER_H
+int add(int a, int b);
+#endif
